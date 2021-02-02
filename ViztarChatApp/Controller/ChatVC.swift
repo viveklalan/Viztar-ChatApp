4a91e1e797d1db4b07402483f269368f544899f5
ms-vivek <vivek.kantilal@markelytics.com>
2021-02-02 23:06:49 +0530

    UI Design done

---
+++ ViztarChatApp/Controller/ChatVC.swift
@@ -0,0 +1,32 @@
+//
+//  ChatVC.swift
+//  ViztarChatApp
+//
+//  Created by Jai Mataji on 02/02/21.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+}
---
+++ ViztarChatApp/Controller/MemberListVC.swift
@@ -0,0 +1,41 @@
+//
+//  MemberListVC.swift
+//  ViztarChatApp
+//
+//  Created by Jai Mataji on 02/02/21.
+//
+
+import UIKit
+
+class MemberListVC: UIViewController {
+
+    @IBOutlet weak var memberListTableView: UITableView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension MemberListVC: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = memberListTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = UIStoryboard.init(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "ChatVC")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+}
